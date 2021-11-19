ffc5cb3ffce4e61fef12572e88147dbc5e5d4534
Nathany <nathanysanti.nm@gmail.com>
2021-11-19 18:47:23 -0300

    first commit

---
+++ while/exercicios/src/exercicio1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class exercicio1 {
+  public static void main(String[] args) {
+    int i = 1;
+
+    while (i <= 50) {
+      System.out.println(i);
+      i = i + 1;
+
+    }
+
+  }
+}
---
+++ while/exercicios/src/exercicio2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class exercicio2 {
+  public static void main(String[] args) {
+    int i = 2;
+
+    while (i <= 100) {
+      System.out.println(i);
+      i = i + 2;
+
+    }
+
+  }
+}
